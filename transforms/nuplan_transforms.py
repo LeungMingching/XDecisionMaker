88c370913dae3dfac672ce0e1b63a74288e8fbf2
Liang Mingjing <liangmingjing849@163.com>
2023-03-08 17:23:29 +0800

    initialize FilterObjectsByRadius
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- transforms/nuplan_transforms.py
+++ transforms/nuplan_transforms.py
@@ -1,18 +1,18 @@
 import numpy as np
 from utils.coordinates_transformation import utm_to_bev
-from utils.math import rotate_vec_2d
+from utils.math import rotate_vec_2d, calculate_distance
 
 class ToBEV(object):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self) -> None:
+        pass
 
     def __call__(self, observation: np.ndarray) -> np.ndarray:
         """ Transform (x, y, heading) of ego and objects from UTM to BEV.
             Transform (v_x, v_y) of objects from UTM to BEV.
 
         Args:
-            observation (np.ndarray): Raw observation
+            observation (np.ndarray): Original observation
             ([x, y, heading, v_x, v_y, a_x, a_y, valid])
 
         Returns:
@@ -35,5 +35,71 @@ class ToBEV(object):
         observation[0, 0:3] = np.zeros(3)
         # for ego (v_x, v_y): already w.r.t. BEV
         # for ego (a_x, a_y): already w.r.t. BEV
+
+        return observation
+
+class FilterObjectsByRadius(object):
+
+    def __init__(self,
+        roi_radius: float,
+        num_objects: int,
+        shuffle_objects: bool = True
+    ) -> None:
+
+        self.roi_radius = roi_radius
+        self.num_objects = num_objects
+        self.shuffle_objects = shuffle_objects
+
+    def __call__(self, observation: np.ndarray) -> np.ndarray:
+        """ Remove virtual objects and objects out of ROI
+            Reshape to (num_objects, features)
+            Shuffle objects (Optional, true by default)
+
+        Args:
+            observation (np.ndarray): Original observation
+
+        Returns:
+            np.ndarray: Filtered observation
+        """
+
+        # filter virtual objects
+        observation = self.filter_virtual_objects(observation)
+
+        # remove far objects
+        observation = self.filter_far_objects(observation)
+
+        # slice or pad
+        observation = self.resize_observation(observation)
+
+        # shuffle
+        observation = self.shuffle_objects(observation)
+        
+        return observation
+    
+    def filter_virtual_objects(self, observation: np.ndarray) -> np.ndarray:   
+        observation_raw = observation.copy()
+        real_selections = observation_raw[:, -1].astype(bool)
+        observation = observation_raw[real_selections, :]
+        return observation
+    
+    def filter_far_objects(self, observation: np.ndarray) -> np.ndarray:
+        observation_raw = observation.copy()
+        ego_loc = observation_raw[0][0:2]
+        distance_array = np.array(
+            [calculate_distance(obj_loc, ego_loc) for obj_loc in observation_raw[1:, 0:2]]
+        )
+        near_selections = distance_array <= self.roi_radius
+        observation = observation_raw[near_selections, :]
+        return observation
+    
+    def resize_observation(self, observation: np.ndarray) -> np.ndarray:
+        observation_raw = observation.copy()
+        # TODO:
+
+        return observation
+    
+    def shuffle_objects(self, observation: np.ndarray) -> np.ndarray:
+        observation_raw = observation.copy()
+        # TODO:
 
         return observation
