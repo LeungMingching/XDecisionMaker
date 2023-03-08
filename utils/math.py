be758104c6089a48cbd09b52cc4ed466f3c90361
Liang Mingjing <liangmingjing849@163.com>
2023-03-08 16:03:39 +0800

    added calculate distance
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- utils/math.py
+++ utils/math.py
@@ -20,3 +20,18 @@ def rotate_vec_2d(
     ])
 
     return np.matmul(rotation_matrix, vec)
+
+def calculate_distance(
+    query_point: np.ndarray,
+    reference_point: np.ndarray
+) -> float:
+    """ Calculate distance between two points of any dimension.
+
+    Args:
+        query_point (np.ndarray): The query point.
+        reference_point (np.ndarray): The reference point.
+
+    Returns:
+        float: distance
+    """
+    return np.linalg.norm(query_point-reference_point)
