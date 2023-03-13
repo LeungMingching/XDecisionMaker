7ac14d546abd9ccbb1b6d13af744a602b910d5c4
Liang Mingjing <liangmingjing849@163.com>
2023-03-13 20:43:15 +0800

    modified metric

--- model/metric.py
+++ model/metric.py
@@ -3,10 +3,10 @@ import torch
 
 def accuracy(output, target):
     with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
+        # pred = torch.argmax(output, dim=1)
+        assert output.shape[0] == len(target)
         correct = 0
-        correct += torch.sum(pred == target).item()
+        correct += torch.sum(output == target).item()
     return correct / len(target)
 
 
